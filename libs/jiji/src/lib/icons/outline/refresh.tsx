1d864ea2a89b448dd2be348729b5d1eeb0c002d8
Aaron McAdam <aaron@aaronmcadam.com>
2021-11-10 15:04:16 +0000

    feat: add refresh icon.

---
+++ libs/jiji/src/lib/icons/outline/refresh.tsx
@@ -0,0 +1,7 @@
+import { RefreshIcon } from '@heroicons/react/outline';
+import { chakra } from '../../chakra/chakra';
+import { baseStyle } from '../base-style';
+
+export const RefreshIconOutline = chakra(RefreshIcon, {
+  baseStyle,
+});
---
+++ libs/jiji/src/lib/icons/solid/refresh.tsx
@@ -0,0 +1,7 @@
+import { RefreshIcon } from '@heroicons/react/solid';
+import { chakra } from '../../chakra/chakra';
+import { baseStyle } from '../base-style';
+
+export const RefreshIconSolid = chakra(RefreshIcon, {
+  baseStyle,
+});
